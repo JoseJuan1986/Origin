8726f734bd1017cce3a121f8fce7ef13bde00c2a
JoseJuan1986 <jojulomacam@gmail.com>
2025-02-20 21:52:25 +0100

    toto list completed

--- scripts/11-exercises.js
+++ scripts/11-exercises.js
@@ -83,6 +83,7 @@ while (j >=0) {
   j--;
 }
 */
+/*
 myArray = [2, 4, 6, 8];
 myArray2 = [];
 for (let i = 0; i < myArray.length; i++) {
@@ -109,3 +110,176 @@ function addOne(array) {
 console.log(addOne(myArray));
 const myArray4 = [-2, -1, 0, 99];
 console.log(addOne(myArray4));
+*/
+/*
+function addNum(array, num) {
+  const myArray5 = [];
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i];
+    const newValue = value + num;
+    myArray5.push(newValue);
+  }
+ 
+  return myArray5;
+    
+}
+
+console.log(addNum([1, 2, 3], 2));
+const test3 = [-2, -1, 0, 99];
+console.log(addNum(test3, 2));
+*/
+/*
+function addArrays(array1, array2) {
+  const myArray6 = [];
+  const total = [];
+  for (let i = 0; i < array1.length; i++) {
+    const value1 = array1[i];
+    const value2 = array2[i];
+    const newValue = value1 + value2;
+    console.log(newValue);
+    myArray6.push(newValue);
+  }
+  return myArray6;
+}
+
+const test2 = [1, 2, 3];
+const test4 = [4, 5, 6];
+console.log(addArrays(test2, test4));
+*/
+/*
+function countPositive(nums) {
+  const myArray7 = [];
+  const total = [];
+  for (let i = 0; i < nums.length; i++) {
+    const value = nums[i];
+      
+    //console.log(value);
+    if (value >= 0) {
+      myArray7.push(value);
+    }
+    
+  }
+
+  return myArray7.length;
+}
+
+const test5 = [1, 33, 5];
+
+console.log(countPositive(test5));
+*/
+/*
+function minMax(nums) {
+  const acumulator = {min: 0, max: 0};
+  //console.log(acumulator);
+  //console.log(acumulator.max);
+  console.log(nums);
+  console.log(nums.length);
+  console.log(nums[0]);
+  
+  if (nums[0] === undefined) {
+    acumulator.min = null;
+    acumulator.max = null;
+    return acumulator; 
+  }
+  
+  if (nums.length === 1) {
+    acumulator.min = nums[0];
+    acumulator.max = nums[0];
+    return acumulator;
+  }
+  
+  for (let i = 0; i < nums.length; i++) {
+    const value = nums[i];
+    
+    if (acumulator.min > value) {
+      acumulator.min = value;
+    }
+    if (acumulator.max < value) {
+      acumulator.max = value;
+    }
+    //console.log(acumulator);
+    
+  }
+  return acumulator;
+}
+
+myArray = [1, -3 , 5];
+console.log(myArray);
+const functionEvaluated = minMax(myArray);
+console.log(functionEvaluated);
+console.log(minMax([-2, 3, -5, 7, 10]));
+myArray2 = [];
+console.log(myArray2);
+console.log(minMax(myArray2));
+myArray3 = [3];
+console.log(myArray3);
+console.log(minMax(myArray3));
+*/
+
+
+
+/*function countWords(words) {
+  let objectCountWords = {fruitName: 'apple', numberOfTimes: 0};
+  console.log(objectCountWords);
+  let acumulator = 0;
+  for (let i = 0; i < words.length; i++) {
+    const value = words[i];
+    objectCountWords.value  
+    if (value === words[i-1] ) {
+      acumulator = acumulator + 1; 
+      objectCountWords = {fruitName: value, numberOfTimes: acumulator};
+    } else {
+      
+      acumulator = 1;
+      objectCountWords = {fruitName: value, numberOfTimes: acumulator};
+    }
+    //objectCountWords = {fruitName: value, numberOfTimes: acumulator};
+    console.log(objectCountWords);
+  }
+  
+  
+  return objectCountWords;
+}
+
+myArrayOfStrings = ['apple', 'grape', 'apple', 'apple'];
+console.log(countWords(myArrayOfStrings));
+*/
+
+function Result(apple, grape) {
+  this.apple = apple;
+  this.grape = grape;
+}
+
+function countWords(words) {
+  const result = {};
+  //const result = new Result();
+  console.log(result);
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    Object.defineProperty(result, word, {value: '0'})
+    console.log(word);
+    //You can add new properties to an existing object by simply giving it a value:
+    // result[word] adds/accesses a property using whatever is
+    // saved inside the 'word' variable.
+    // If word = 'apple', result[word] will do result['apple']
+    // If word = 'grape', result[word] will do result['grape']
+    //result[word] = 3;
+    console.log(result[word]);
+    
+    console.log(result);
+    result[word]++;
+    /*
+    if (!result[word]) {
+      result[word] = 1;
+    } else {
+      result[word]++;
+    }
+    //console.log(result);
+    console.log(result[word]);
+  */
+    }
+    
+  return result;
+}
+
+console.log(countWords(['apple', 'grape', 'apple', 'apple']));
--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,10 @@
- const todoList = ['make dinner', 'wash dishes'];
+ const todoList = [{
+  name: 'make dinner',
+  dueDate: '19-02-2025' 
+}, {
+  name: 'wash dishes', 
+  dueDate:'19-02-2025'
+}];
 
  renderTodoList();
 
@@ -6,23 +12,44 @@
     let todoListHTML = '';
     //generating the html technique
     for (i = 0; i < todoList.length; i++) {
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        const { name, dueDate } = todoObject;
+        const html = `
+          <div>${name}</div>
+          <div>${dueDate}</div>
+          <button onclick="
+            todoList.splice(${i}, 1);
+            renderTodoList();
+          " class="delete-todo-button">Delete</button>
+        `;
         todoListHTML += html;
 
     }
-    console.log(todoListHTML);
+    //console.log(todoListHTML);
 
     document.querySelector('.js-todo-list')
       .innerHTML = todoListHTML;
  }
  function addTodo() {
-  const inputElement = document.querySelector('.js-name-input');
-  const name = inputElement.value;
-  todoList.push(name);
-  console.log(todoList);
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
 
-  inputElement.value = '';
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    
+    const dueDate = dateInputElement.value;
+    
+      
+    todoList.push({
+      //name: name,
+      //dueDate: dueDate
+      name,
+      dueDate
+    });
+    //console.log(todoList);
 
-  renderTodoList();
+    inputElement.value = '';
+
+    renderTodoList();
  }
